e63f1b2d7e9887e722bc9ca59edc5224b93f7f3c
Charlélie <chlopez.sio@gmail.com>
2023-08-23 14:28:59 +0200

    Update CI workflow to run tests on main and charlelie branch

--- __tests_/addressModel.test.js
+++ __tests_/addressModel.test.js
@@ -1,5 +1,53 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const Address = require('../models/addressModel');
+const RealCompany = jest.requireActual('../models/companyModel'); // Import the real Company model
+RealCompany.sync = jest.fn(); // Mock the sync function
+
+const mockCreate = jest.fn();
+
+// Mock the Company model
+jest.mock('../models/companyModel', () => {
+  return {
+    create: mockCreate,
+    // Add other methods if necessary
+  };
+});
+
+describe('Registration Process', () => {
+  let Company; // declare the Company variable
+
+  beforeEach(() => {
+    Company = require('../models/companyModel'); // require the mocked Company model here
+    mockCreate.mockClear();
+  });
+
+  test('registers a new company', async () => {
+    const companyData = {
+      name: 'Test Company',
+      mail: 'test@test.com',
+      pwd: 'test1234'  // Note: In actual scenarios, you'd hash this before saving
+    };
+
+    // Mock successful creation
+    mockCreate.mockResolvedValueOnce(companyData);
+
+    // Simulate registration (this part will vary based on your registration logic)
+    const registeredCompany = await Company.create(companyData);
+
+    // Check if the create method was called
+    expect(mockCreate).toHaveBeenCalled();
+
+    // Check if the right data was saved
+    expect(registeredCompany.name).toBe(companyData.name);
+    expect(registeredCompany.mail).toBe(companyData.mail);
+    // Add more assertions for other fields
+
+    // Note: You might also want to test error scenarios, like if the email already exists
+  });
+
+  // Add more tests for other scenarios, like failed registration due to validation errors
+});
+
 
 // Mock Sequelize instance and model
 jest.mock('sequelize', () => {
@@ -39,5 +87,47 @@ describe('Address Model', () => {
     expect(address.country).toBe(addressData.country);
   });
 
+
   // Add more tests for other Address model functionalities (updating, querying, etc.)
+});
+
+describe('Company Model Validity', () => {
+  test('should not create a company without a name', async () => {
+    expect.assertions(1);
+
+    try {
+      await RealCompany.create({
+        mail: 'no-name@test.com',
+        pwd: 'test1234'
+      });
+    } catch (error) {
+      expect(error.message).toContain('Company.name cannot be null');
+    }
+  });
+
+  test('should not create a company without an email', async () => {
+    expect.assertions(1);
+
+    try {
+      await RealCompany.create({
+        name: 'No Email Co.',
+        pwd: 'test1234'
+      });
+    } catch (error) {
+      expect(error.message).toContain('Company.mail cannot be null');
+    }
+  });
+
+  test('should not create a company without a password', async () => {
+    expect.assertions(1);
+
+    try {
+      await RealCompany.create({
+        name: 'No Password Co.',
+        mail: 'no-pwd@test.com'
+      });
+    } catch (error) {
+      expect(error.message).toContain('Company.pwd cannot be null');
+    }
+  });
 });
