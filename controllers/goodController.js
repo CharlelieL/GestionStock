c1cfc31419297f1ad5c02d0d8470953c34f7a5a5
Negi63 <florian.fournier63@gmail.com>
2023-08-25 15:28:53 +0200

    add style login/register

--- controllers/goodController.js
+++ controllers/goodController.js
@@ -1,14 +1,15 @@
 const good = require('../models/goodModel');
 const passport = require('passport');
+
+
 class goodController{
 
-showALL(response,request){
-    if (request.isAthentificated()) {
+showALL(request, response){
+    
         const goods= good.findAll()
-        response.render('goods',{goods})
-    }else{
-        response.redirect('/')
-    }
+        response.render('dashboard',{goods})
+    
 
 }
 }
+module.exports = new goodController()
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,16 +1,19 @@
 // routes/dashboard.js
 const express = require('express');
+const goodController = require('../controllers/goodController');
 const router = express.Router();
+
 
 // Dashboard route handler
 router.get('/dashboard', (req, res) => {
   // Check if the user is authenticated using Passport's isAuthenticated() method
   if (req.isAuthenticated()) {
     // Render the dashboard view
+    // goodController.showALL()
     res.render('dashboard'); // Assuming you have a file named "dashboard.ejs" in the views folder
   } else {
     // Redirect unauthenticated users to the login page
-    res.redirect('/loginView'); // Change this to match the actual URL for your login page
+    res.redirect('/login'); // Change this to match the actual URL for your login page
   }
 });
 
