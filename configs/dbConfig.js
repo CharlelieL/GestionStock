e3ba83819d49e42448ace317f423ae7ae85fcac1
CharlelieL <chlopez.sio@gmail.com>
2023-09-22 10:32:51 +0200

    reworked dbConfig.js, for proper connection to Azure db

--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -1,6 +1,5 @@
- const { Sequelize } = require('sequelize');
- const fs = require('fs');
-
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
 
 console.log("DB User:", process.env.DATABASE_USER);
 console.log("DB Password:", process.env.DATABASE_PASSWORD);
@@ -8,19 +7,18 @@ console.log("DB Host:", process.env.DATABASE_HOST);
 console.log("DB Port:", process.env.DATABASE_PORT);
 console.log("DB Name:", process.env.DATABASE_NAME);
 
-const encodedUser = encodeURIComponent(process.env.DATABASE_USER);
-const encodedPassword = encodeURIComponent(process.env.DATABASE_PASSWORD);
-
-const dbConnectionString = `mysql://${encodedUser}:${encodedPassword}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?sslca=${__dirname}/../ssl/DigiCertGlobalRootCA.crt.pem`;
-
-console.log(dbConnectionString);
-
-const sequelize = new Sequelize(dbConnectionString, {
+const sequelize = new Sequelize({
+    username: `${process.env.DATABASE_USER}@${process.env.DATABASE_HOST}`,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: 'mysql',
     logging: console.log,
     dialectOptions: {
         ssl: {
-            rejectUnauthorized: false
+            rejectUnauthorized: false,
+            ca: fs.readFileSync(__dirname + '/../ssl/DigiCertGlobalRootCA.crt.pem')
         }
     }
 });
