db55157167918d4a712c5c05e1d8bcbccde5a68d
CharlelieL <chlopez.sio@gmail.com>
2023-09-25 15:12:33 +0200

    corrected gothub actions

--- azure-migration/grafana.tf
+++ azure-migration/grafana.tf
@@ -1,57 +1,20 @@
-resource "azurerm_managed_grafana" "example" {
-  name                = "example-managedgrafana"
-  location            = azurerm_resource_group.rg.location
+resource "azurerm_dashboard_grafana" "grafana" {
+  name                = "myGrafanaDashboard"
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "Gen1_Standard"
-  custom_domain_name_enabled = false
+  location            = "West Europe"
+  api_key_enabled                   = true
+  deterministic_outbound_ip_enabled = true
+  public_network_access_enabled     = false
 
   identity {
     type = "SystemAssigned"
   }
 
-  storage_account {
-    name = azurerm_storage_account.grafana_storage.name
-    key  = azurerm_storage_account.grafana_storage.primary_access_key
-  }
-
-  authentication {
-    type        = "AzureAD"
-    client_id   = "azuread-application-client-id"
-    client_secret {
-      name    = "client-secret-name"
-      version = "client-secret-version"
-    }
-    tenant_id = var.ARM_TENANT_ID
-  }
-  
-  lifecycle {
-    ignore_changes = [identity[0].principal_id, identity[0].tenant_id]
-  }
-}
-
-output "grafana_url" {
-  value       = azurerm_managed_grafana.example.data_source_grafana_url
-  description = "The URL to access the Grafana instance"
-}
-
-
-resource "azurerm_storage_account" "grafana_storage" {
-  name                     = "grafanastorageacct"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"  # This sets it to General Purpose v2
-  access_tier              = "Hot"        # Sets the access tier to Hot
-  
   tags = {
-    environment = "production"
+    env = "production"
   }
 }
-
-resource "azurerm_storage_container" "grafana_container" {
-  name                  = "grafana-data"
-  storage_account_name  = azurerm_storage_account.grafana_storage.name
-  container_access_type = "private"
+output "grafana_url" {
+  value       = azurerm_dashboard_grafana.grafana.url
+  description = "The Grafana dashboard URL"
 }
-
